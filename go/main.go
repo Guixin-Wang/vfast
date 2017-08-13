5cf85f3c6711b96eac74d0aa1307529b785f34ad
dylanwww <wgxhadoop@163.com>
2017-08-13 14:48:04 +0800

    helloworld
    Signed-off-by: dylanwww <wgxhadoop@163.com>

--- go/main.go
+++ go/main.go
@@ -4,14 +4,13 @@ import "fmt"
 
 func main(){
 	println("hello world")
-
 	i:=1
 	for i<10 {
 		println(i)
 		i++
 	}
 	for j:=1;j<5;j++{
-		println("heloo world")
+		println("hello world")
 	}
 	for {
 		fmt.Println("loop")
@@ -20,6 +19,17 @@ func main(){
 	fmt.Print("vfast")
 	boolean:=true
 	for boolean {
-		boolean:=false
+		fmt.Println("aaaaaaaa")
+		break
+	}
+	sum :=0
+	for i:=1;i<=1000;i++{
+		sum +=i
+
+	}
+	println(sum)
+	for i,j := 20,20 ;i<30 && j < 30;i++{
+		println(j," ",i)
+		j += 2
 	}
 }
