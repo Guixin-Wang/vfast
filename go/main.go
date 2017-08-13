7a8bc01002afbc0e84263919884d3c6dbd0d908e
dylanwww <wgxhadoop@163.com>
2017-08-13 14:33:22 +0800

    helloworld
    Signed-off-by: dylanwww <wgxhadoop@163.com>

--- go/main.go
+++ go/main.go
@@ -1,5 +1,21 @@
-package _go
+package main
+
+import "fmt"
 
 func main(){
 	println("hello world")
+
+	i:=1
+	for i<10 {
+		println(i)
+		i++
+	}
+	for j:=1;j<5;j++{
+		println("heloo world")
+	}
+	for {
+		fmt.Println("loop")
+		break
+	}
+	fmt.Print("vfast")
 }
